a68d8ea0da9de61b42bc3253ed7d36e687592176
Asma-M06 <asmamadhvaswala52@gmail.com>
2026-01-15 21:02:58 +0530

    Move from in-memory object to redis for distributed rate limiting

--- src/middleware/rateLimiter.js
+++ src/middleware/rateLimiter.js
@@ -1,40 +1,50 @@
-const RATE_LIMIT = 5;
-const requestStore = {};
+const redis = require("redis");
+const client = redis.createClient();
 
-const rateLimiter = (req, res, next) => {
-    const apiKey = req.headers["x-api-key"];
-    if (!apiKey) {
-        return res.status(401).json({ message: "Api key missing" })
+//connet to redis
+(async () => {
+    client.connect();
+})();
+
+const rateLimiter = async (req, res, next) => {
+    const apiKey = req.header["x-api-key"];
+    if (!apiKey) return res.status(401).json({ message: "Api key missing" })
+
+
+    const tier = {
+        'free': { limit: 5, windowMS: 60000 },
+        'pro': { limit: 100, windowMS: 60000 }
+    };
+
+    const userTier = apiKey === "pro-secret-key" ? 'pro' : 'free';
+    const { limit, windowMS } = tier[userTier];
+
+    const now = Date.now();
+    const key = ` rate_limit:${apiKey}`;
+
+    try {
+        await client.zRemRangeByScore(key, 0, now - windowMS);
+        const requestCount = await client.zCard(key);
+
+        if (requestCount >= limit) {
+            return res.status(429).json(
+                {
+                    message: "Rate limit exceeded",
+                    tier: userTier,
+                    limit: limit
+                }
+            );
+        }
+
+        await client.zAdd(key, { score: now, value: now.toString() });
+        await client.expire(key, Math.floor(windowMS / 1000));
+
+        next();
     }
-
-
-    const currentTime = Date.now();
-
-    if (!requestStore[apiKey]) {
-        requestStore[apiKey] = {
-            count: 1,
-            startTime: currentTime
-        };
-        return next();
+    catch (err) {
+        console.error("Redis error : ", err);
+        next();
     }
-
-    const elapsedTime = currentTime - requestStore[apiKey].startTime;
-
-    if (elapsedTime > 24 * 60 * 60 * 1000) {
-        requestStore[apiKey] = {
-            count: 1,
-            startTime: currentTime
-        };
-        return next();
-    }
-
-    if (requestStore[apiKey].count > RATE_LIMIT) {
-        return res.status(429).json({ message: "Rate limit exceeded" })
-    }
-
-    requestStore[apiKey].count++;
-    next();
-
 };
 
 module.exports = rateLimiter;
